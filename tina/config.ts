79babf7a27611d12959a7ef5741d9829b24b3e7d
mwaxman519 <115736601+mwaxman519@users.noreply.github.com>
2025-01-26 23:31:16 +0000

    kjn;kj

--- tina/config.ts
+++ tina/config.ts
@@ -41,10 +41,62 @@ export default defineConfig({
             required: true,
           },
           {
-            type: "string",
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "rich-text",
             name: "body",
             label: "Body",
             isBody: true,
+          },
+        ],
+      },
+      {
+        name: "global",
+        label: "Global",
+        path: "content/global",
+        format: "json",
+        ui: {
+          global: true,
+        },
+        fields: [
+          {
+            name: "header",
+            label: "Header",
+            type: "object",
+            fields: [
+              {
+                type: "string",
+                name: "name",
+                label: "Site Name",
+              },
+              {
+                type: "object",
+                name: "nav",
+                label: "Navigation",
+                list: true,
+                ui: {
+                  itemProps: (item) => ({
+                    label: item?.label,
+                  }),
+                },
+                fields: [
+                  {
+                    type: "string",
+                    name: "label",
+                    label: "Label",
+                  },
+                  {
+                    type: "string",
+                    name: "href",
+                    label: "Link",
+                  },
+                ],
+              },
+            ],
           },
         ],
       },
